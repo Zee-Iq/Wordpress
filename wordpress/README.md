7be82298097eb931f8a7f0534cb57795274359b4
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-03-21 14:36:01 +0100

    first commit , initializing project

---
+++ wordpress/README.md
@@ -0,0 +1,1 @@
+# Creating a wysiwyg editor (Wordpress clone)
---
+++ wordpress/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+const App = () => {
+  return(
+    <div>Hello from client</div>
+  )
+}
+export default App;
+
+
+
