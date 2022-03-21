722fdf6236be47f3c293f509298bbf5bd628db6e
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-03-21 15:02:25 +0100

    added logic to throw new error when process.env.DB_URI is not available

--- server/config/db.js
+++ server/config/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 module.exports = async () => {
     try {
+        if(!process.env.DB_URI) throw new Error("Please specify mongoDB connection string in .env file as DB_URI") 
         mongoose.connect(process.env.DB_URI)
         console.log('Connected to DB!')
     } catch (error) {
