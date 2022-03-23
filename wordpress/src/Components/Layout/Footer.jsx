ea72a7cb5074fe2e15654903d5826b15f142cb4a
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-03-23 13:20:29 +0100

    created landing page and footer

---
+++ wordpress/src/Components/Layout/Footer.jsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <div>Hello from Footer.jsx</div>;
+};
+
+export default Footer;
---
+++ wordpress/src/Components/Layout/LandingPage.jsx
@@ -0,0 +1,9 @@
+const LandingPage = () => {
+  return <div>Hello from Landing Page </div>;
+
+  <img srx="https://unsplash.com/photos/cckf4TsHAuw"/>
+};
+
+export default LandingPage
+
+
