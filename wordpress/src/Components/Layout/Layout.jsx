68661a599ab6dc5f49dcf336bc8e8cce9f5ce18d
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-03-23 13:20:06 +0100

    created Layout.jsx

---
+++ wordpress/src/Components/Layout/Layout.jsx
@@ -0,0 +1,17 @@
+import Footer from "./Footer"
+import Header from "./Header"
+import LandingPage from "./LandingPage"
+
+
+const Layout = () =>{
+    return (
+
+        <div>
+            <Header />
+            <LandingPage />
+            <Footer />
+        </div>
+    )
+}
+
+export default Layout
