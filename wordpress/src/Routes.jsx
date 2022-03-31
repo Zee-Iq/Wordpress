8d2c7cda73c96591b2b023a578a458f756c6264a
ChristopherA81 <cdavid81@gmail.com>
2022-03-31 15:06:20 +0200

    Added Sidebar to Routes.jsx

--- wordpress/src/Routes.jsx
+++ wordpress/src/Routes.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import { Layout } from "./components/Layout/Layout";
 import Login from "./components/login/Login";
 import Register from "./components/RegisterForm/Register";
+import Sidebar from "../src/components/Sidebar";
+import FAQ from "../src/components/FAQ";
 
 export const Routes = () => {
   return <Switch>
@@ -10,6 +12,8 @@ export const Routes = () => {
     <Route path='/' exact component={Layout} />
     <Route path='/login' exact component={Login} />
     <Route path='/register' exact component={Register} />
+    <Route path='/sidebar' exact component={Sidebar} />
+    <Route path='/faq' exact component={FAQ} />
     
 
   </Switch>;
