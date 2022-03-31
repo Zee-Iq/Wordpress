191fcbd37dd7cf12afcab2a34f354823c333db95
ChristopherA81 <cdavid81@gmail.com>
2022-03-31 15:07:34 +0200

    Imported Sidebar.jsx and FAQ.jsx to Layout

--- wordpress/src/components/Layout/Layout.jsx
+++ wordpress/src/components/Layout/Layout.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { Header } from '../Header/Header'
 import Login from '../login/Login'
 import Register from '../RegisterForm/Register'
+import Sidebar from '../Sidebar'
+import FAQ from '../FAQ'
+
 
 
 export const Layout = () => {
@@ -9,6 +12,8 @@ export const Layout = () => {
       <>
     <Header />
     <Register />
+    <Sidebar />
+    <FAQ />
     
     </>
   )
