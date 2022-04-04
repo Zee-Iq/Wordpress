0bef013195b8de717c2178e4c04c5cb3b543026b
Victor Ajagunna <victor-ajagunna@hotmail.com>
2022-04-04 15:41:22 +0200

    Changes to Contact made

--- wordpress/src/components/Home/Home.jsx
+++ wordpress/src/components/Home/Home.jsx
@@ -1,19 +1,15 @@
 import './Home.css';
 import React from 'react';
 
-// import Sidebar from '..Sidebar/Sidebar';
 import Button from '@mui/material/Button';
-import { Avatar } from '@mui/material';
+
 
 export default function Home() {
   return (
     <div className='homeContainer'>
-     
-      {/* <Sidebar /> */}
-     
-        <div className='overlay'>
+      <div className='overlay'>
           <div className='homeText'>
-            {/* <h1>iBelieve</h1> */}
+            {/* <h1>Our App Title and Logo</h1> */}
             <h3>Let's Share your Space. The only blog that broadens your horizon and makes you know, see and be the change you desire in yourself and the community!</h3>
             </div>
         <Button variant="contained" href="../login/">
