c78e3593a69000442550175838ce981f5c8a9964
ChristopherA81 <cdavid81@gmail.com>
2022-04-05 14:30:05 +0200

    New Changes

--- wordpress/src/components/FAQ/Faq.jsx
+++ wordpress/src/components/FAQ/Faq.jsx
@@ -1,78 +1,100 @@
 import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-// import Grid from '@mui/material/Grid';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
 import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import './FAQ.css'
 
 
+export default function FAQ() {
+  const [expanded, setExpanded] = React.useState(false);
 
-export default function Faq() {
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
   return (
-    <div style={{ height: 350, width: '100%' }}>
-      <CssBaseline />
-      <Container maxWidth="md">
-        <Box >
-        <Typography 
-            variant="h4" gutterBottom gutterTop
-            md={{ width: '100%', flexShrink: 0 }}>
-            Frequently Asked Questions
-        </Typography>
-        <Divider />
-        <Typography variant='h8'
-            gutterBottom marginLeft={4}
-            sx={{ width: '100%', flexShrink: 0, padding: '5' }}
-        > 
-        <h2>What is our App and Our Mission about?</h2>
-        <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus adipisci provident maxime molestiae possimus velit, ullam blanditiis hic sed fuga odio obcaecati voluptatibus, voluptates iure minus unde, sunt nihil eaque.</p>
-        
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt distinctio nisi quis nemo, atque omnis cupiditate sunt, officiis voluptatem nesciunt voluptates. Architecto suscipit beatae sit alias obcaecati, repudiandae magni dignissimos?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-
-        </Typography>
-        <br/>
-        <Divider />
-        <Typography variant='h8'
-            gutterBottom marginLeft={4}
-            sx={{ width: '100%', flexShrink: 0 }}
-        > 
-        <h2>Is the Blog free or by Subscription?</h2>
-        <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus adipisci provident maxime molestiae possimus velit, ullam blanditiis hic sed fuga odio obcaecati voluptatibus, voluptates iure minus unde, sunt nihil eaque.</p>
-        
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt distinctio nisi quis nemo, atque omnis cupiditate sunt, officiis voluptatem nesciunt voluptates. Architecto suscipit beatae sit alias obcaecati, repudiandae magni dignissimos?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-        </Typography>
-        <Divider />
-        <Typography variant='h8'
-            gutterBottom marginLeft={4}
-            sx={{ width: '100%', flexShrink: 0 }}
-        > 
-        <h2>How to send Blog Articles?</h2>
-        <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus adipisci provident maxime molestiae possimus velit, ullam blanditiis hic sed fuga odio obcaecati voluptatibus, voluptates iure minus unde, sunt nihil eaque.</p>
-        
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt distinctio nisi quis nemo, atque omnis cupiditate sunt, officiis voluptatem nesciunt voluptates. Architecto suscipit beatae sit alias obcaecati, repudiandae magni dignissimos?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-
-        </Typography>
-        <Divider />
-        <Typography variant='h8'
-            gutterBottom marginLeft={4}
-            sx={{ width: '100%', flexShrink: 0 }}
-        > 
-        <h2>Reporting Blog Articles?</h2>
-        <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus adipisci provident maxime molestiae possimus velit, ullam blanditiis hic sed fuga odio obcaecati voluptatibus, voluptates iure minus unde, sunt nihil eaque.</p>
-        
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt distinctio nisi quis nemo, atque omnis cupiditate sunt, officiis voluptatem nesciunt voluptates. Architecto suscipit beatae sit alias obcaecati, repudiandae magni dignissimos?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-
-        </Typography>
-        </Box>
-      </Container>
+    <div>
+      <div className='accordion'>
+      
+      <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}
+        backgroundColor="green">
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1bh-content"
+          id="panel1bh-header"
+      
+        >
+          <Typography sx={{ width: '33%', flexShrink: 0 }}>
+          <h3> Our Goal</h3> 
+          </Typography>
+          <Typography sx={{ color: 'text.secondary' }}> <h3> About our App</h3> 
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          This Blog API is a   
+            student project presented as part of our learning output at DCI Web design program. We believe great software should work with minimum set up, so you can focus on sharing our story, productions and freely, our abilities. It is simple and predictable so the user can easily get started. It also offers powerful features for growth and successful learning.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2bh-content"
+          id="panel2bh-header"
+          backgroundColor="green"
+        >
+          <Typography sx={{ width: '33%', flexShrink: 0 }}><h3> Getting Started</h3></Typography>
+          <Typography sx={{ color: 'text.secondary' }}>
+          <h3> How to gain full access to the App's features</h3>
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus,
+            varius pulvinar diam eros in elit. Pellentesque convallis laoreet
+            laoreet.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3bh-content"
+          id="panel3bh-header"
+        >
+          <Typography sx={{ width: '33%', flexShrink: 0 }}>
+          <h3> Advanced settings</h3> 
+          </Typography>
+          <Typography sx={{ color: 'text.secondary' }}>
+            Filtering has been entirely disabled for whole web server
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
+            amet egestas eros, vitae egestas augue. Duis vel est augue.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel4bh-content"
+          id="panel4bh-header"
+        >
+          <Typography sx={{ width: '33%', flexShrink: 0 }}>Personal data</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
+            amet egestas eros, vitae egestas augue. Duis vel est augue.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      </div>
     </div>
   );
 }
